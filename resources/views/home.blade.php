c859e10ec51842fbe73819a7aa7d8fc27fbef81c
Mujadid Jundi Fauzi <mujadidjundifauzi200@gmail.com>
2023-11-27 09:43:15 +0700

    Video Profil & Beberapa Perubahan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,35 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="row w-100 sh position-relative">
-    <div class="col-5 bg-main"></div>
-    <div class="col-1 bg-sec"></div>
-    <div class="col-3 position-relative ps-0 pe-0">
-        <p class="gt text-uppercase position-absolute fs-1 sbk-style">smart bengkel komputer</p>
+<div class="position-relative">
+    <div class="row w-100 sh position-relative">
+        <div class="col-5 bg-main g-1"></div>
+        <div class="col-1 bg-sec"></div>
+        <div class="col-3 position-relative ps-0 pe-0">
+            <p class="gt text-uppercase position-absolute fs-1 sbk-style">smart bengkel komputer</p>
+        </div>
+        <div class="col-2 bg-main g-2"></div>
+        <div class="col-1 bg-sec"></div>
     </div>
-    <div class="col-2 bg-main"></div>
-    <div class="col-1 bg-sec"></div>
+    <img class="grafiti-style position-absolute w-100" src="{{  asset('assets/grafiti_sbk.png') }}" alt="Grafiti SBK"
+        style="top: 53%">
+    <div class="video">
+        <video autoplay loop class="position-relative w-100 behind">
+            <source src="{{ asset('assets/testing.mp4') }}" type="video/mp4">
+            Please update your browser version
+        </video>
+    </div>
+    <p class="text-uppercase position-absolute bottom-0 fs-2 ms-3">
+        <span class="sec">bersama</span>
+        sbk<br>
+        <span class="sec">bangkitkan</span>
+        <span>smk</span>
+    </p>
+    <a href="">
+        <p class="bg-main text-uppercase position-absolute bottom-0 end-0 about-us-style">about us</p>
+    </a>
 </div>
-<div class="grafiti-style w-100">
-    <img width="100%" src="{{  asset('assets/grafiti_sbk.png') }}" alt="Grafiti SBK">
+<div>
+    
 </div>
 @endsection
