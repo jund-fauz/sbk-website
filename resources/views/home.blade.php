75f1d7b133d5e684cf6df8f81eef0b63bb5459a7
Mujadid Jundi Fauzi <mujadidjundifauzi200@gmail.com>
2023-12-16 21:45:55 +0700

    Perfection the video profil section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,19 +13,21 @@
     </div>
     <img class="grafiti-style position-absolute w-100" id="grafiti-sbk" src="{{  asset('assets/grafiti_sbk.png') }}"
         alt="Grafiti SBK">
-    <video autoplay loop class="position-relative w-100 behind video">
-        <source src="{{ asset('assets/testing.mp4') }}" type="video/mp4">
-        Please update your browser version
-    </video>
-    <p class="text-uppercase position-absolute bottom-0 fs-2 ms-3">
-        <span class="sec">bersama</span>
-        <span class="main">sbk</span><br>
-        <span class="sec">bangkitkan</span>
-        <span class="bt">smk</span>
-    </p>
-    <a href="">
-        <p class="bg-main text-uppercase position-absolute bottom-0 end-0 about-us-style">about us</p>
-    </a>
+    <div class="position-relative video-container">
+        <video autoplay loop class="w-100 video">
+            <source src="{{ asset('assets/testing.mp4') }}" type="video/mp4">
+            Please update your browser version
+        </video>
+        <p class="text-uppercase position-absolute bottom-0 fs-2 margin-when-large">
+            <span class="sec">bersama</span>
+            <span class="main">sbk</span><br>
+            <span class="sec">bangkitkan</span>
+            <span class="bt">smk</span>
+        </p>
+        <a href="">
+            <p class="bg-main text-uppercase position-absolute bottom-0 end-0 about-us-style">about us</p>
+        </a>
+    </div>
 </div>
 <script src="{{ asset('js/home.js') }}"></script>
 @endsection
