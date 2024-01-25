e33fe83f58406f1fab5654d052a5227665bccf31
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-01-25 12:39:54 +0700

    Some Improvements

--- views/navbar.js
+++ views/navbar.js
@@ -1,10 +1,12 @@
-const navbar = `<a href="index.html"><img class="position-absolute top-50 translate-middle-y" src="pictures/sbk-navbar.png" alt="SBK"></a>
+const navbar = `<a href="index.html"><img class="position-absolute top-50" src="pictures/sbk-navbar.png" alt="SBK"></a>
         <div class="position-absolute top-50 start-50 translate-middle d-flex">
             <a href="#">mikrotik</a>
             <a href="#">learn basic</a>
             <a href="#">member</a>
             <a href="#">xtra</a>
         </div>
-        <i class="fa-solid fa-cart-shopping position-absolute top-50 end-0 translate-middle-y"></i>`
+        <a href="#">
+            <i class="fa-solid fa-cart-shopping position-absolute top-50 translate-middle-y"></i>
+        </a>`
 
 export default navbar
