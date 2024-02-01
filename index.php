f64af9ed054db117590d556cce550582c9f78ae4
jund-fauz <mujadidjundifauzi200@gmail.com>
2024-02-01 07:21:18 +0700

    Some Improvements

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="stylesheet/main.css?<?= time(); ?>">
-    <link rel="stylesheet" href="stylesheet/index.css?<?= time(); ?>">
+    <!-- <link rel="stylesheet" href="stylesheet/index.css?<?= time(); ?>"> -->
     <link rel="stylesheet" href="stylesheet/member.css?<?= time(); ?>">
 </head>
 
--- pages/member.php
+++ pages/member.php
@@ -1,5 +1,10 @@
 <div class="position-relative mt-2">
-    <img src="pictures/devit-cache.png" alt="Ustadz Devit Hari Ashari">
+    <img src="pictures/devit-cache.png" alt="Ustadz Devit Hari Ashari" class="mt-2 ms-4">
     <h1 class="title text-center position-absolute top-0 w-100">penanggung jawab</h1>
     <p class="text-black text-capitalize position-absolute top-50 start-50 translate-middle">devit hari ashari, s.pd.</p>
+</div>
+<div class="position-relative mt-5">
+    <img src="pictures/devit-cache.png" alt="Ustadz Devit Hari Ashari" class="mt-2 ms-4">
+    <h1 class="title text-center position-absolute top-0 w-100">pembina</h1>
+    <p class="text-black text-capitalize position-absolute top-50 start-50 translate-middle">diantika arifianti, s.t.</p>
 </div>
